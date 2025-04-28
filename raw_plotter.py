62582ec2baf40a6a585bce125fb33f533a163de9
Patxi <fmunguiawulftange@ucsd.edu>
2025-04-27 18:58:44 -0700

    plotting plasma state

--- raw_plotter.py
+++ raw_plotter.py
@@ -1,5 +1,4 @@
 from utils.plotting import PlasmaPlotter
-
 import h5py
 import numpy as np
 
@@ -7,8 +6,8 @@ import numpy as np
 plotter = PlasmaPlotter(save_path="./figs")
 
 # 2. Load your data
-frame_idx = 1        # Frame to plot
-nx, ny = 128, 89       # Number of points in x and y (from paper description)
+frame_idx = 1            # Frame to plot
+nx, ny = 128, 89         # Number of points in x and y
 points_per_frame = nx * ny
 
 start_idx = frame_idx * points_per_frame
@@ -20,19 +19,13 @@ with h5py.File("data/plasma_data.h5", "r") as f:
     y_den = f['y_den'][start_idx:end_idx]
     y_Te = f['y_Te'][start_idx:end_idx]
 
-# 3. Call the plotting function
+# 3. Plot plasma state
 plotter.plot_plasma_state(
     X0=x_x,
     X1=x_y,
     y_den=y_den,
     y_Te=y_Te,
-    N_time=0,        # 0 because we already sliced one frame
-    len_skip=1,      # 1 since no skipping in sliced data
+    N_time=0,        # Already sliced frame
+    len_skip=1,      # No skip in data
     len_2d=points_per_frame
-)
-
-# 4. Plot 1d - electric potential 
-plotter.plot_1d_potential(
-    X0= x_x, X1=x_y, y_plot: List[np.ndarray],
-                         len_loop_y=1, inds: np.ndarray)
 )
